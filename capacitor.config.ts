8268f9e4c0c3973416ee36208c723a316935245f
hnmnhnmn <hnmnhnmn2@gmail.com>
2022-10-17 11:03:05 +0700

    capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.huy.todolist',
+  appName: 'To Do List Web to Android',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
